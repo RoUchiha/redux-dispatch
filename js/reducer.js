a0ffb057d27a9078b973eb0662c3f3d73588c9bd
RoUchiha <swaggsikh@gmail.com>
2022-05-15 18:58:59 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,15 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
 
-changeState(state, action)
+let state = {count: 0}
+
+
+function render() {
+  document.body.textContent = state.count;
+}
+
